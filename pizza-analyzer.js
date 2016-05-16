e53b2a18947640ef296731d7f363e48ee72fd736
Pokegear <christoph.ding@gmail.com>
2016-05-16 16:18:17 -0400

    Pizza analyzer works.

--- pizza-analyzer.js
+++ pizza-analyzer.js
@@ -3,33 +3,40 @@ var request = require('request');
 
 var toppingsURL = 'http://files.olo.com/pizzas.json';
 
-var tallyToppings = function (toppingsList) {
+var pizzaAnalyzer = function () {
+  this.orders = {};
+  this.output = [];
+}
 
+pizzaAnalyzer.prototype.tallyToppings = function (toppingsList) {
   for (var i = 0; i < toppingsList.length; i++) {
-    var toppings = sample[i].toppings;
+    var toppings = toppingsList[i].toppings;
+    for (var j = 0; j < toppings.length; j++) {
+      var currentTopping = toppings[j];
+      if (this.orders[currentTopping]) {
+        this.orders[currentTopping]++;
+      } else {
+        this.orders[currentTopping] = 1;
+      }
+    }    
+  }
+}
 
+pizzaAnalyzer.prototype.topTwenty = function () {
+  var orders = this.orders;
+  toppingsSorted = Object.keys(orders).sort(function(a,b){return orders[b]-orders[a]});
+  for (var i = 0; i < toppingsSorted.length; i++) {
+    var currentTopping = toppingsSorted[i];
+    this.output.push({[currentTopping]: this.orders[currentTopping]})
+  }
+  return this.output;
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-};
-
-request(toppingsURL, function (error, res, body) {
-    if (!error && res.statusCode ==200) {
-      console.log(body);
+request(toppingsURL, function (error, res, pizzaData) {
+    if (!error && res.statusCode == 200) {
+      var parsedPizzaData = JSON.parse(pizzaData);
+      var currentPizzaAnalzyer = new pizzaAnalyzer();
+      currentPizzaAnalzyer.tallyToppings(parsedPizzaData);
+      console.log(currentPizzaAnalzyer.topTwenty());
     }
 });
